4533af2428bbbae1ec126918b9f8e334129d5a2f
Iris1902 <irispicis1962@gmail.com>
2025-07-06 21:08:27 -0500

    fix: Correct route order for health check and product show endpoint in api.php

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
 Route::get('/products-read', [ProductController::class, 'index']);
-Route::get('/products-read/{id}', [ProductController::class, 'show']);
 Route::get('/products-read/health', [ProductController::class, 'healthCheck']);
+Route::get('/products-read/{id}', [ProductController::class, 'show'])->where('id', '[0-9]+');
